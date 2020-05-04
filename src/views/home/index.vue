c5f20141ca50a20b725130b64e59dcb515aea138
a506221439 <57707049+a506221439@users.noreply.github.com>
2020-05-04 15:07:46 +0800

    txx

---
+++ src/views/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home-index">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homeIndex',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
