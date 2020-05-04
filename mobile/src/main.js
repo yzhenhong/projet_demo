b91c5dc7337245dce27c188ed1df44b8ca8f2810
v_grandyang <wx_a05b6a43fd254c1dbecca0dde1b4cf68@git.code.tencent.com>
2020-05-04 19:00:12 +0800

    修改端口

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -1,20 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
 import store from '@/store'
-import axios from './utils/net'
+import ajax from '@/utils/ajax'
+
 import VueWechatTitle from 'vue-wechat-title'
+
 import vant from 'vant'
 import 'vant/lib/index.css';
 import 'vant/lib/icon/local.css';
 
 
+import App from './App'
+
+
 Vue.config.productionTip = false
 
-Vue.prototype.$axios = axios
-Vue.prototype.$net = axios
+// Global methods
+Vue.prototype.$ajax = ajax
+Vue.prototype.$net = ajax
+
 
 Vue.use(vant)
 Vue.use(VueWechatTitle)
